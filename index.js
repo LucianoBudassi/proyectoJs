3ec4270d607a030a9cf8a03ebbb3715ca33fa5a2
LucianoBudassi <lucianobudassi@hotmail.com>
2022-07-04 21:13:23 -0300

    primer entrega

--- index.js
+++ index.js
@@ -5,6 +5,75 @@ let titulo = "Almacen de Bebidas";
 let totalCombos = 5;
 let totalCompra = 0;
 
+let precioCombo1 = 1050;
+let precioCombo2 = 2900;
+let precioCombo3 = 950;
+let precioCombo4 = 1500;
+let precioCombo5 = 2500;
+
+class Producto{
+        constructor(id, combo , ){
+                this.id = id;
+                this.combo = combo;
+        }
+}
+
+const combosProductos = [];
+combosProductos.push(new Producto(1, "Fernet + CocaCola"));
+combosProductos.push(new Producto(2, "Absolut + Red Bull"));
+combosProductos.push(new Producto(3, "Campari + Citric"));
+combosProductos.push(new Producto(4, "Alamos + Otro Loco Mas"));
+combosProductos.push(new Producto(5, "Herederos + Scheweppes"));
+
+let nombre = "";
+
+const catalogo = () => {
+        let mensaje = titulo;
+        mensaje = mensaje + "\r \n";
+        mensaje = mensaje + combosProductos[1] + "$" + precioCombo1;
+        mensaje = mensaje + "\r \n";
+        mensaje = mensaje + combosProductos[2] + "$" + precioCombo2;
+        mensaje = mensaje + "\r \n";
+        mensaje = mensaje + combosProductos[3] + "$" + precioCombo3;
+        mensaje = mensaje + "\r \n";
+        mensaje = mensaje + combosProductos[4] + "$" + precioCombo4;
+        mensaje = mensaje + "\r \n";
+        mensaje = mensaje + combosProductos[5] + "$" + precioCombo5;
+        mensaje = mensaje + "\r \n";
+    
+        return (mensaje);
+    }
+
+    const carroCompra = (producto, cantidad) => {
+
+        let mensaje = " ";
+
+    switch (producto) {
+        case "1" : mensaje = combosProductos[1];
+                totalCompra = totalCompra + parseInt (combosProductos[1] * cantidad); 
+                break;
+        
+        case "2" : mensaje = combosProductos[2];
+                totalCompra = totalCompra + parseInt (combosProductos[2] * cantidad); 
+                break;
+        
+        case "3" : mensaje = combosProductos[3];
+                totalCompra = totalCompra + parseInt (combosProductos[3] * cantidad); 
+                break;
+        
+        case "4" : mensaje = combosProductos[4];
+                totalCompra = totalCompra + parseInt (combosProductos[4] * cantidad); 
+                break;
+        
+        case "5" : mensaje = combosProductos[5];
+                totalCompra = totalCompra + parseInt (combosProductos[5] * cantidad); 
+                break;
+
+        }
+    
+
+
+/*  
 let combo1 = 1;
 let combo2 = 2;
 let combo3 = 3;
@@ -24,7 +93,7 @@ let precioCombo4 = 1500;
 let precioCombo5 = 2500;
 
 let nombre = "";
-
+ 
 const catalogo = () => {
     let mensaje = titulo;
     mensaje = mensaje + "\r \n";
@@ -41,11 +110,12 @@ const catalogo = () => {
 
     return (mensaje);
 }
-
+ */
+/*
 const carroCompra = (producto, cantidad) => {
 
     let mensaje = " ";
-
+ 
     switch (producto) {
         case "1" : mensaje = parseInt(combo1) + " " + comboFernet + "$" + parseInt(precioCombo1);
                 totalCompra = totalCompra + parseInt (precioCombo1 * cantidad); 
@@ -68,15 +138,15 @@ const carroCompra = (producto, cantidad) => {
                 break;
 
         }
-    
+     */
 }
 
 nombre = prompt("Ingrese su nombre"); 
-let respuesta = "email";
+let respuesta = 'SI';
 
-respuesta = prompt(`Hola ${nombre} ingresa tu email para ingresar a nuestro catalogo`);
+respuesta = prompt(`Hola ${nombre} desea acceder a nuestro catalogo de compras? \r \n SI \r \n NO \r \n SALIR \r \n `);
 
-while (repuesta = "email") {
+while (respuesta === 'SI') {
     let producto = prompt(catalogo());
     let cantidad = prompt(`Seleccion la cantidad ${nombre}`);
     
@@ -84,10 +154,19 @@ while (repuesta = "email") {
     alert("Total de tu compra" + " " + parseInt(totalCompra));
 
     respuesta = prompt(`${nombre} Deseas seguir comprando?`);
-    if (respuesta == "NO");
-    alert("Gracias");
-    break;
-}
+    if (respuesta === 'NO' && (totalCompra = 0)) {
+        alert('No realizaste ninguna compra');
+      }
+    }
+    
+    alert(
+      'Gracias ' +
+        nombre +
+        ' \r\nEl total de tu compra es $' +
+        parseInt(totalCompra)
+    );
+
+
 
 
 
