aaca64813193b41b3ff20f9421f64cc5cf584b5b
LucianoBudassi <lucianobudassi@hotmail.com>
2022-07-10 15:35:50 -0300

    html+json+eventos

--- index.js
+++ index.js
@@ -1,6 +1,81 @@
 /* Creamos un catalogo de almacen de bebidas que contenga 5 productos diferentes
 Solicitamos al usuario elegir su combo y las cantidades del mismo */
 
+const cards = document.getElementById('cards');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+const templateFooter = document.getElementById('template-footer').content;
+const teompleteCarrito = document.getElementById('template-carrito').content
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+        fetchData()
+})
+cards.addEventListener('click', e =>{
+        addCarrito(e)
+})
+
+const fetchData = async () => {
+        try { 
+                const res = await fetch ('index.json')
+                const data = await res.json()
+                /* console.log(data);*/
+                pintarCards(data);
+        } catch (error) {
+                console.log(error);
+        }
+}
+
+const pintarCards = data =>{
+        data.forEach(producto => {
+                templateCard.querySelector('h5').textContent = producto.title
+                templateCard.querySelector('p').textContent = producto.precio
+                templateCard.querySelector('img').setAttribute("src",producto.thumbnailUrl)
+                templateCard.querySelector('.btn-dark').dataset.id= producto.id
+
+
+                
+                
+                const clone = templateCard.cloneNode(true)
+                fragment.appendChild(clone)
+        })
+        cards.appendChild(fragment);        
+
+}
+
+const addCarrito = e => {
+       
+        if (e.target.classList.contains('btn-dark')){
+           setCarrito(e.target.parentElement);
+
+        }
+        e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+      const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+        }
+        if(carrito.hasOwnProperty(producto.id)){
+                producto.cantidad = carrito[producto.id].cantidad + 1
+        }
+
+        carrito[producto.id] = {... producto}
+        console.log(carrito);
+
+}
+
+const pintarCarrito = () => {
+        console.log(carrito);
+}
+
+
+/* 
 let titulo = "Almacen de Bebidas";
 let totalCombos = 5;
 let totalCompra = 0;
@@ -71,75 +146,6 @@ const catalogo = () => {
                 break;
 
         }
-    
-
-
-/*  
-let combo1 = 1;
-let combo2 = 2;
-let combo3 = 3;
-let combo4 = 4;
-let combo5 = 5;
-
-let comboFernet = "Fernet + CocaCola";
-let comboVodka = "Absolut + Red Bull";
-let comboCampari = "Campari + Citric";
-let comboVino = "Alamos + Otro Loco Mas";
-let comboGin = "Herederos + Scheweppes";
-
-let precioCombo1 = 1050;
-let precioCombo2 = 2900;
-let precioCombo3 = 950;
-let precioCombo4 = 1500;
-let precioCombo5 = 2500;
-
-let nombre = "";
- 
-const catalogo = () => {
-    let mensaje = titulo;
-    mensaje = mensaje + "\r \n";
-    mensaje = mensaje + parseInt(combo1) + "-" + comboFernet + "$" + parseInt(precioCombo1);
-    mensaje = mensaje + "\r \n";
-    mensaje = mensaje + parseInt(combo2) + "-" + comboVodka + "$" + parseInt(precioCombo2);
-    mensaje = mensaje + "\r \n";
-    mensaje = mensaje + parseInt(combo3) + "-" + comboCampari + "$" + parseInt(precioCombo3);
-    mensaje = mensaje + "\r \n";
-    mensaje = mensaje + parseInt(combo4) + "-" + comboVino + "$" + parseInt(precioCombo4);
-    mensaje = mensaje + "\r \n";
-    mensaje = mensaje + parseInt(combo5) + "-" + comboGin + "$" + parseInt(precioCombo5);
-    mensaje = mensaje + "\r \n";
-
-    return (mensaje);
-}
- */
-/*
-const carroCompra = (producto, cantidad) => {
-
-    let mensaje = " ";
- 
-    switch (producto) {
-        case "1" : mensaje = parseInt(combo1) + " " + comboFernet + "$" + parseInt(precioCombo1);
-                totalCompra = totalCompra + parseInt (precioCombo1 * cantidad); 
-                break;
-        
-        case "2" : mensaje = parseInt(combo2) + " " + comboVodka + "$" + parseInt(precioCombo2);
-                totalCompra = totalCompra + parseInt (precioCombo2 * cantidad); 
-                break;
-        
-        case "3" : mensaje = parseInt(combo3) + " " + comboCampari + "$" + parseInt(precioCombo3);
-                totalCompra = totalCompra + parseInt (precioCombo3 * cantidad); 
-                break;
-        
-        case "4" : mensaje = parseInt(combo4) + " " + comboVino + "$" + parseInt(precioCombo4);
-                totalCompra = totalCompra + parseInt (precioCombo4 * cantidad); 
-                break;
-        
-        case "5" : mensaje = parseInt(combo5) + " " + comboGin + "$" + parseInt(precioCombo5);
-                totalCompra = totalCompra + parseInt (precioCombo5 * cantidad); 
-                break;
-
-        }
-     */
 }
 
 nombre = prompt("Ingrese su nombre"); 
@@ -169,7 +175,7 @@ while (respuesta === 'SI') {
         parseInt(totalCompra)
     );
 
-
+ */
 
 
 
